520347f5bc55ce38d841ac492ee1f1c194fa2761
rhutik parab <51870483+CodeBlind31@users.noreply.github.com>
2019-10-27 00:01:11 +0530

    changed kernerl random_state
    changed random_state from 0 to 1

--- Machine Learning/linear regression/simple_linear_regression.py
+++ Machine Learning/linear regression/simple_linear_regression.py
@@ -12,7 +12,7 @@ y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 1)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -28,8 +28,7 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
-
+Y_predict = regressor.predict(X_test)
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
